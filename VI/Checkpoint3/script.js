b6562732aaa481acd483ec65a488e7d2a491cc1c
Beatriz Paulo <beatriz.paulo@tecnico.ulisboa.pt>
2024-10-07 20:12:05 +0100

    Changed dashboard name

--- VI/Checkpoint3/script.js
+++ VI/Checkpoint3/script.js
@@ -165,19 +165,19 @@ var globalData1, globalData2, globalData3;
 const buttons = document.querySelectorAll('.menu-btn');
 
 function init() {
-  d3.csv("dataset1_salary_employment_ratio.csv").then(function (data1) {
+  d3.csv("datasets/dataset1_salary_employment_ratio.csv").then(function (data1) {
     globalData1 = data1.map(d => {
       d.year = +d.year; 
       return d;
     }).sort((a, b) => a.year - b.year); // Sort by year in ascending order
 
-    d3.csv("dataset2_job_category.csv").then(function (data2) {
+    d3.csv("datasets/dataset2_job_category.csv").then(function (data2) {
        // Convert 'year' to number and sort the data by 'year'
        globalData2 = data2.map(d => {
         d.year = +d.year; // Convert year to a number
         return d;
       }).sort((a, b) => a.year - b.year); // Sort by year in ascending order
-    d3.csv("average_ratio_employ_by_continent.csv").then(function(data3){
+    d3.csv("datasets/average_ratio_employ_by_continent.csv").then(function(data3){
       globalData3 = data3.map(d => { 
         d.year = +d.year;
         return d;
